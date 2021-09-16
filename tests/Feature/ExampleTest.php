703449a8cb74092ad23594cb9b5679a7b20bc50b
Robert Kujawa <robert.k@incfile.com>
2021-09-16 17:00:40 -0500

    Prepare phpunit

---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace RKujawa\LaravelPaymentGateway\Tests;
+
+use Orchestra\Testbench\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function test_test_passes()
+    {
+        $this->assertTrue(true);
+    }
+}
