22b709ff81707473092b7bca83c1d269578ac781
Robert Kujawa <robert.k@incfile.com>
2021-10-04 16:14:43 -0500

    Add artisan command that generates payment type migrations

--- src/PaymentServiceProvider.php
+++ src/PaymentServiceProvider.php
@@ -3,12 +3,19 @@
 namespace rkujawa\LaravelPaymentGateway;
 
 use Illuminate\Support\ServiceProvider;
+use rkujawa\LaravelPaymentGateway\Console\Commands\AddPaymentType;
 
 class PaymentServiceProvider extends ServiceProvider
 {
     public function boot()
     {
         $this->loadMigrationsFrom(__DIR__.'/Database/Migrations');
+
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                AddPaymentType::class,
+            ]);
+        }
     }
 
     public function register()
