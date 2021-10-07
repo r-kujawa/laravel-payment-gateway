583be920e92c8e1ede461a285648d702a16ff073
Mario Avila <maar01art@gmail.com>
2021-10-07 12:30:12 -0500

    Fix to GatewayRequestContract

--- src/Interfaces/GatewayRequestContract.php
+++ src/Interfaces/GatewayRequestContract.php
@@ -4,28 +4,28 @@ namespace rkujawa\LaravelPaymentGateway\Interfaces;
 
 interface GatewayRequestContract
 {
-    public function createCustomerProfile(BillableContract $client): GatewayResponse;
-    public function deleteCustomerProfile(string $customerToken): GatewayResponse;
-    public function getCustomerProfile(string $customerToken): GatewayResponse;
+    public function createCustomerProfile(BillableContract $client): GatewayResponseContract;
+    public function deleteCustomerProfile(string $customerToken): GatewayResponseContract;
+    public function getCustomerProfile(string $customerToken): GatewayResponseContract;
 
     public function updateCustomerProfile(
         string $clientId,
         string $customerToken,
         string $email,
         string $description
-    ): GatewayResponse;
+    ): GatewayResponseContract;
 
-    public function createPaymentMethod(int $paymentCustomerId, PaymentTypeContract $paymentMethod): GatewayResponse;
-    public function deletePaymentMethod(string $customerToken, string $paymentToken): GatewayResponse;
-    public function getPaymentMethod(string $customerToken, string $paymentToken): GatewayResponse;
+    public function createPaymentMethod(int $paymentCustomerId, PaymentTypeContract $paymentMethod): GatewayResponseContract;
+    public function deletePaymentMethod(string $customerToken, string $paymentToken): GatewayResponseContract;
+    public function getPaymentMethod(string $customerToken, string $paymentToken): GatewayResponseContract;
 
     public function updatePaymentMethod(
         string $customerToken,
         string $paymentToken,
         PaymentTypeContract $paymentType
-    ): GatewayResponse;
+    ): GatewayResponseContract;
 
-    public function charge(PaymentTypeContract $payment, int $amount, string $description): GatewayResponse;
-    public function void(): GatewayResponse;
-    public function refund(): GatewayResponse;
+    public function charge(PaymentTypeContract $payment, int $amount, string $description): GatewayResponseContract;
+    public function void(): GatewayResponseContract;
+    public function refund(): GatewayResponseContract;
 }
