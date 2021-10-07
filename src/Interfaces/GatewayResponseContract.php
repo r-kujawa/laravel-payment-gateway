bbc858970f821b11be825fbf780ba472f3447c96
Mario Avila <maar01art@gmail.com>
2021-10-07 13:26:10 -0500

    Adding code blocks and comment for further discussion.

--- src/Interfaces/GatewayResponseContract.php
+++ src/Interfaces/GatewayResponseContract.php
@@ -2,21 +2,49 @@
 
 namespace rkujawa\LaravelPaymentGateway\Interfaces;
 
+/**
+ * To discuss: Maybe this contract should be smaller and only have duty to comunicate errors and status of transaction
+ * (successful or not)
+ */
 interface GatewayResponseContract
 {
+    /**
+     * @return bool
+     */
     public function isSuccessful(): bool;
 
+    /**
+     * @return bool
+     */
     public function isApproved(): bool;
 
+    /**
+     * @return string
+     */
     public function getRawResponse(): string;
 
+    /**
+     * @return string
+     */
     public function getErrorMessage(): string;
 
+    /**
+     * @return int
+     */
     public function getErrorCode(): int;
 
+    /**
+     * @return bool
+     */
     public function isDuplicateProfile(): bool;
 
+    /**
+     * @return mixed
+     */
     public function getTransactionId();
 
+    /**
+     * @return string
+     */
     public function getAvsCode(): string;
 }
