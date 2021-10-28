4b730eb16bd5c9ad2a4ad41d31e89583a7e85756
Robert Kujawa <robert.k@incfile.com>
2021-10-28 14:37:40 -0500

    Bind the payment services to the container

--- src/PaymentServiceProvider.php
+++ src/PaymentServiceProvider.php
@@ -4,6 +4,8 @@ namespace rkujawa\LaravelPaymentGateway;
 
 use Illuminate\Support\ServiceProvider;
 use rkujawa\LaravelPaymentGateway\Console\Commands\AddPaymentType;
+use rkujawa\LaravelPaymentGateway\Interfaces\PaymentManager;
+use rkujawa\LaravelPaymentGateway\Interfaces\PaymentProcessor;
 
 class PaymentServiceProvider extends ServiceProvider
 {
@@ -19,15 +21,25 @@ class PaymentServiceProvider extends ServiceProvider
         }
     }
 
-    public function register()
-    {
-
-    }
-
     protected function vendorPublish()
     {
         $this->publishes([
             __DIR__ . '/config/payment.php' => config_path('payment.php'),
         ], 'payment-config');
+    }
+
+    public function register()
+    {
+        $this->app->bind(PaymentGateway::class, function ($app) {
+            return new PaymentGateway(PaymentService::FULL);
+        });
+
+        $this->app->bind(PaymentProcessor::class, function ($app) {
+            return new PaymentService(PaymentService::PROCESSOR);
+        });
+
+        $this->app->bind(PaymentManager::class, function ($app) {
+            return new PaymentService(PaymentService::MANAGER);
+        });
     }
 }
