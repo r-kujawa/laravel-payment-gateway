25a51685295114020a9d59632b52f4d51f172e90
Robert Kujawa <robert.k@incfile.com>
2021-10-28 14:34:45 -0500

    Add payment processing and management traits

--- src/Traits/ManagesPayments.php
+++ src/Traits/ManagesPayments.php
@@ -1,26 +1,35 @@
 <?php
 
-namespace rkujawa\LaravelPaymentGateway\Interfaces;
+namespace rkujawa\LaravelPaymentGateway\Traits;
 
-interface PaymentManagement
+trait ManagesPayments
 {
     /**
      * @param \rkujawa\LaravelPaymentGateway\Interfaces\BillableContract $billable
      * @param array|mixed $data
      * @return \rkujawa\LaravelPaymentGateway\Interfaces\PaymentManagerResponse
      */
-    public function createPaymentMethod($billable, $data);
+    public function createPaymentMethod($billable, $data)
+    {
+        return $this->gateway->createPaymentMethod($billable, $data);
+    }
 
     /**
      * @param \rkujawa\LaravelPaymentGateway\Models\PaymentMethod $paymentMethod
      * @param array|mixed $data
      * @return \rkujawa\LaravelPaymentGateway\Interfaces\PaymentManagerResponse
      */
-    public function updatePaymentMethod($paymentMethod, $data);
+    public function updatePaymentMethod($paymentMethod, $data)
+    {
+        return $this->gateway->updatePaymentMethod($paymentMethod, $data);
+    }
     
     /**
      * @param \rkujawa\LaravelPaymentGateway\Models\PaymentMethod $paymentMethod
      * @return \rkujawa\LaravelPaymentGateway\Interfaces\PaymentManagerResponse
      */
-    public function deletePaymentMethod($paymentMethod);
+    public function deletePaymentMethod($paymentMethod)
+    {
+        return $this->gateway->deletePaymentMethod($paymentMethod);
+    }
 }
