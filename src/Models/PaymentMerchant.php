a8d4096f3e40b10a219dc388185455081ac18db9
r-kujawa <bobkujawa@gmail.com>
2021-12-23 23:14:00 -0600

    Add merchant table, model and factory

---
+++ src/Models/PaymentMerchant.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace rkujawa\LaravelPaymentGateway\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use rkujawa\LaravelPaymentGateway\Database\Factories\PaymentMerchantFactory;
+
+class PaymentMerchant extends Model
+{
+    use HasFactory;
+
+    protected $guarded = ['id'];
+
+    /**
+     * Create a new factory instance for the model.
+     *
+     * @return \Illuminate\Database\Eloquent\Factories\Factory
+     */
+    protected static function newFactory()
+    {
+        return PaymentMerchantFactory::new();
+    }
+
+    public function wallets()
+    {
+        return $this->hasMany(config('payment.models.' . Wallet::class, Wallet::class), 'merchant_id');
+    }
+
+    public function payment_methods()
+    {
+        return $this->hasManyThrough(config('payment.models.' . PaymentMethod::class, PaymentMethod::class), config('payment.models.' . Wallet::class, Wallet::class), 'merchant_id');
+    }
+
+    public static function slugify($name)
+    {
+        return preg_replace('/[^a-z0-9]+/i', '_', trim(strtolower($name)));
+    }
+}
