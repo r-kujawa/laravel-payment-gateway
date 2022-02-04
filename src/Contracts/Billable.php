06bb0bf80077abf7119aca77f599c09503d242a7
Robert Kujawa <robert.k@incfile.com>
2022-02-04 09:34:34 -0600

    Update the Billable contract

--- src/Contracts/Billable.php
+++ src/Contracts/Billable.php
@@ -4,12 +4,5 @@ namespace rkujawa\LaravelPaymentGateway\Contracts;
 
 interface Billable
 {
-    /**
-     * Get the billable's identifier.
-     *
-     * @return string
-     */
-    public function getBillableKey();
-
     // TODO: Add methods like charge, refund, etc...
 }
