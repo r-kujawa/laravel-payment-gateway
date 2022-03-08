edc279eb758bf551ccd21e8429d22587a16f8b62
Robert Kujawa <robert.k@incfile.com>
2022-03-08 10:13:45 -0600

    Add DocBlocks to Payment facade

--- src/Facades/Payment.php
+++ src/Facades/Payment.php
@@ -5,6 +5,28 @@ namespace rkujawa\LaravelPaymentGateway\Facades;
 use Illuminate\Support\Facades\Facade;
 use rkujawa\LaravelPaymentGateway\PaymentGateway;
 
+/**
+ * @method static \rkujawa\LaravelPaymentGateway\PaymentGateway provider($provider)
+ * @method static \rkujawa\LaravelPaymentGateway\Models\PaymentProvider getProvider()
+ * @method static void setProvider($provider)
+ * @method static string|int|\rkujawa\LaravelPaymentGateway\Models\PaymentProvider getDefaultProvider()
+ * @method static \rkujawa\LaravelPaymentGateway\PaymentGateway merchant($merchant)
+ * @method static \rkujawa\LaravelPaymentGateway\Models\PaymentMerchant getMerchant()
+ * @method static void setMerchant($merchant, $strict = true)
+ * @method static string|int getDefaultMerchant()
+ * @method static \rkujawa\LaravelPaymentGateway\PaymentResponse getWallet(\rkujawa\LaravelPaymentGateway\Models\Wallet $wallet)
+ * @method static \rkujawa\LaravelPaymentGateway\PaymentResponse getPaymentMethod(\rkujawa\LaravelPaymentGateway\Models\PaymentMethod $paymentMethod)
+ * @method static \rkujawa\LaravelPaymentGateway\PaymentResponse tokenizePaymentMethod(\rkujawa\LaravelPaymentGateway\Contracts\Billable $billable, $data)
+ * @method static \rkujawa\LaravelPaymentGateway\PaymentResponse updatePaymentMethod(\rkujawa\LaravelPaymentGateway\Models\PaymentMethod $paymentMethod, $data)
+ * @method static \rkujawa\LaravelPaymentGateway\PaymentResponse removePaymentMethod(\rkujawa\LaravelPaymentGateway\Models\PaymentMethod $paymentMethod)
+ * @method static \rkujawa\LaravelPaymentGateway\PaymentResponse authorize($data, \rkujawa\LaravelPaymentGateway\Contracts\Billable $billable = null)
+ * @method static \rkujawa\LaravelPaymentGateway\PaymentResponse capture(\rkujawa\LaravelPaymentGateway\Models\PaymentTransaction $transaction, $data = [])
+ * @method static \rkujawa\LaravelPaymentGateway\PaymentResponse authorizeAndCapture($data, \rkujawa\LaravelPaymentGateway\Contracts\Billable $billable = null)
+ * @method static \rkujawa\LaravelPaymentGateway\PaymentResponse void(\rkujawa\LaravelPaymentGateway\Models\PaymentTransaction $transaction, $data = [])
+ * @method static \rkujawa\LaravelPaymentGateway\PaymentResponse refund(\rkujawa\LaravelPaymentGateway\Models\PaymentTransaction $transaction, $data = [])
+ * 
+ * @see \rkujawa\LaravelPaymentGateway\PaymentGateway
+ */
 class Payment extends Facade
 {
     /**
