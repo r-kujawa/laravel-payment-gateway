976238b9c532e84cc0e2d48422122291b7f706c1
Robert Kujawa <bobkujawa@gmail.com>
2022-11-27 08:16:15 -0600

    Reformat payment.php config

--- src/config/payment.php
+++ src/config/payment.php
@@ -4,22 +4,6 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Payment Defaults
-    |--------------------------------------------------------------------------
-    |
-    | This option controls the default payment "provider" for your application.
-    | You may set these defaults as required, you should set your primary payments
-    | provider here, as it will be the provider to be injected automatically.
-    |
-    */
-    'defaults' => [
-        'driver' => 'database',
-        'provider' => '',
-        'merchant' => '',
-    ],
-
-    /*
-    |--------------------------------------------------------------------------
     | Payment Test Mode
     |--------------------------------------------------------------------------
     |
@@ -28,21 +12,7 @@ return [
     | usefull when you are running tests in a CI/CD environment.
     |
     */
-    'test_mode' => env('PAYMENT_TEST_MODE', false),
-
-    /*
-    |--------------------------------------------------------------------------
-    | Payment Test Configuration
-    |--------------------------------------------------------------------------
-    |
-    | The following configurations will only be considered when test_mode
-    | is set to true. Here you may set any configurations needed in
-    | order to have your tests running smoothly.
-    |
-    */
-    'test' => [
-        'gateway' => \App\Services\Payment\TestPaymentGateway::class,
-    ],
+    'test_mode' => false,
 
     /*
     |--------------------------------------------------------------------------
@@ -55,38 +25,8 @@ return [
     |
     */
     'drivers' => [
+        'config' => \rkujawa\LaravelPaymentGateway\Drivers\ConfigDriver::class,
         'database' => \rkujawa\LaravelPaymentGateway\Drivers\DatabaseDriver::class,
     ],
-
-    /*
-    |--------------------------------------------------------------------------
-    | Payment Provider Configurations
-    |--------------------------------------------------------------------------
-    |
-    | Here you may provide the location of your provider's gateway implementation
-    | by specifying the 'gateway' key for each provider and mapping the class to
-    | it, you may also add any provider specific configuration you might have.
-    |
-    */
-    'providers' => [
-        // 'example' => [
-        //     'gateway' => \App\Services\Payment\ExamplePaymentGateway::class,
-        // ],
-    ],
-
-    /*
-    |--------------------------------------------------------------------------
-    | Payment Models
-    |--------------------------------------------------------------------------
-    |
-    | This option allows you to override the models to be used for managing payments.
-    | You may define your own models that extend the ones of the package in order to
-    | keep your relationships in sync with your own models.
-    |
-    */
-    // 'models' => [
-    //     \rkujawa\LaravelPaymentGateway\Models\PaymentMethod::class => \App\Models\Payment\PaymentMethod::class,
-    //     \rkujawa\LaravelPaymentGateway\Models\Wallet::class => \App\Models\Payment\Wallet::class,
-    // ],
 
 ];
