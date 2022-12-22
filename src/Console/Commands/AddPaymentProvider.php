a40ee89b025c4879448b8df2d562f6ecfa5937e6
Robert Kujawa <bobkujawa@gmail.com>
2022-12-22 06:56:00 -0600

    Stop generating migrations for new providers

--- src/Console/Commands/AddPaymentProvider.php
+++ src/Console/Commands/AddPaymentProvider.php
@@ -4,13 +4,11 @@ namespace rkujawa\LaravelPaymentGateway\Console\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Str;
-use InvalidArgumentException;
-use rkujawa\LaravelPaymentGateway\Models\PaymentProvider;
-use rkujawa\LaravelPaymentGateway\Traits\GeneratesMigrations;
+use rkujawa\LaravelPaymentGateway\Traits\GeneratesFiles;
 
 class AddPaymentProvider extends Command
 {
-    use GeneratesMigrations;
+    use GeneratesFiles;
 
     /**
      * The name and signature of the console command.
@@ -19,8 +17,7 @@ class AddPaymentProvider extends Command
      */
     protected $signature = 'payment:add-provider
                             {provider? : The payment provider name}
-                            {--slug= : The payment provider dev name}
-                            {--skip-migration : Do not run the migration}
+                            {--id= : The payment provider identifier}
                             {--test : Generates a gateway to be used for testing purposes}';
 
     /**
@@ -28,15 +25,15 @@ class AddPaymentProvider extends Command
      *
      * @var string
      */
-    protected $description = 'Add a new payment provider and scaffold it\'s implementation.';
+    protected $description = 'Scaffold a new payment provider\'s gateway and response classes.';
 
     /**
      * The payment provider attributes to be saved.
      *
      * @var string $name
-     * @var string $slug
+     * @var string $id
      */
-    protected $name, $slug;
+    protected $name, $id;
 
     /**
      * Execute the console command.
@@ -47,20 +44,16 @@ class AddPaymentProvider extends Command
     {
         $this->setProperties();
 
-        $studlySlug = Str::studly($this->slug);
-
-        if (! $this->option('test')) {
-            $this->generateProviderMigration($studlySlug);
-        }
+        $provider = Str::studly($this->id);
 
         $this->putFile(
-            app_path("Services/Payment/{$studlySlug}PaymentGateway.php"),
-            $this->makeFile(__DIR__ . '/../stubs/payment-gateway.stub', ['name' => $studlySlug])
+            app_path("Services/Payment/{$provider}PaymentGateway.php"),
+            $this->makeFile(__DIR__ . '/../stubs/payment-gateway.stub', ['name' => $provider])
         );
 
         $this->putFile(
-            app_path("Services/Payment/{$studlySlug}PaymentResponse.php"),
-            $this->makeFile(__DIR__ . '/../stubs/payment-response.stub', ['name' => $studlySlug])
+            app_path("Services/Payment/{$provider}PaymentResponse.php"),
+            $this->makeFile(__DIR__ . '/../stubs/payment-response.stub', ['name' => $provider])
         );
     }
 
@@ -79,36 +72,11 @@ class AddPaymentProvider extends Command
             )
         );
 
-        $this->slug = PaymentProvider::slugify(
-            $this->option('slug') ??
-            $this->ask("What slug would you like to use for the {$this->name} payment provider?", PaymentProvider::slugify($this->name))
-        );
-    }
-
-    private function generateProviderMigration($studlySlug)
-    {
-        $migrationClass = "Add{$studlySlug}PaymentProvider";
-
-        if ($this->classExists($migrationClass, $this->getMigrationPath())) {
-            throw new InvalidArgumentException("{$migrationClass}::class already exists.");
-        }
-
-        $this->putFile(
-            $this->generateMigrationFilePath($migrationClass),
-            $this->makeFile(
-                __DIR__ . '/../stubs/payment-provider-migration.stub',
-                [
-                    'class' => $migrationClass,
-                    'name' => addslashes($this->name),
-                    'slug' => $this->slug,
-                ]
-            )
-        );
-
-        $this->info('The migration to add ' . $this->name . ' payment provider has been generated.');
-
-        if ((! $this->option('skip-migration')) && $this->confirm('Would you like to run the migration?', true)) {
-            $this->call('migrate', ['--force']);
-        }
+        $this->id =
+            $this->option('id') ??
+            $this->ask(
+                "How would you like to identify the {$this->name} payment provider?",
+                preg_replace('/[^a-z0-9]+/i', '_', trim(strtolower($this->name)))
+            );
     }
 }
