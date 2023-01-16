da1717c7abeff30c6c451d390788153cde66be8a
Robert Kujawa <robert.kujawa@proton.me>
2023-01-16 06:13:06 -0600

    Fix & add docblocks

--- src/Traits/Questionable.php
+++ src/Traits/Questionable.php
@@ -4,11 +4,24 @@ namespace rkujawa\LaravelPaymentGateway\Traits;
 
 trait Questionable
 {
+    /**
+     * Ask for the name of the payment entity (provider, merchant, etc...) to be added.
+     *
+     * @param string $entity
+     * @return string
+     */
     protected function askName($entity)
     {
         return $this->ask("What payment {$entity} would you like to add?");
     }
 
+    /**
+     * Ask for the id of the payment entity (provider, merchant, etc...) to be added.
+     *
+     * @param string $entity
+     * @param string $name
+     * @return string
+     */
     protected function askId($entity, $name)
     {
         return $this->ask(
